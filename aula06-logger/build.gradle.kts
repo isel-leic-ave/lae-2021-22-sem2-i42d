a803b21ca8dc00732f8d5b04dc15942cf1ac6d78
Miguel Gamboa <miguelgamboa@outlook.com>
2022-03-16 15:48:38 +0000

    Update gradle.build to kotlin 1.5. Add configuration for tests output.

--- aula06-logger/build.gradle.kts
+++ aula06-logger/build.gradle.kts
@@ -8,10 +8,17 @@
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.4.20"
+    id("org.jetbrains.kotlin.jvm") version "1.5.20"
 
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+}
+
+// Test Logging
+tasks.withType<Test> {
+    testLogging {
+        events("started", "passed", "skipped", "failed")
+    }
 }
 
 repositories {

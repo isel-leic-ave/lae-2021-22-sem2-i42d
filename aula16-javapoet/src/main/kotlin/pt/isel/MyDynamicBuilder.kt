aa9eec8d7ceca37a9593b556afc13e6fc93ec498
Miguel Gamboa <miguelgamboa@outlook.com>
2022-04-18 13:09:34 +0100

    Setup lesson 16 and 17

---
+++ aula16-javapoet/src/main/kotlin/pt/isel/MyDynamicBuilder.kt
@@ -0,0 +1,11 @@
+package pt.isel
+
+import com.squareup.javapoet.FieldSpec
+import com.squareup.javapoet.JavaFile
+import com.squareup.javapoet.MethodSpec
+import com.squareup.javapoet.TypeSpec
+import javax.lang.model.element.Modifier
+
+fun buildMyDynamic(): JavaFile {
+    TODO()
+}
---
+++ aula17-logger-dynamic/src/test/kotlin/pt/isel/LoggerTest.kt
@@ -0,0 +1,46 @@
+package pt.isel
+
+import pt.isel.Student.Address
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class PrinterStringBuffer : Printer {
+    val buffer = StringBuffer()
+    override fun print(msg: Any?) {
+        buffer.append(msg)
+    }
+}
+
+class LoggerTest {
+    @Test fun testLogPointProperties() {
+        val expected = "pt.isel.Point {x = 5,y = 7,}${System.lineSeparator()}"
+        val p = Point(5, 7)
+        val out = PrinterStringBuffer()
+        val logger = Logger(out)
+        logger.log(p)
+        assertEquals(expected, out.buffer.toString())
+    }
+
+    @Test fun testLogAccountProperties() {
+        val expected = "pt.isel.SavingsAccount {balance = 1000,}${System.lineSeparator()}"
+        val a = SavingsAccount(1000, 2.5)
+        val out = PrinterStringBuffer()
+        val logger = Logger(out, LoggerKind.PROPERTIES)
+        logger.log(a)
+        assertEquals(expected, out.buffer.toString())
+    }
+
+    @Test fun testLogAccountPropertiesAndFunctions() {
+        val expected = "pt.isel.SavingsAccount {balance() = 1000,monthlyInterest() = 208,}${System.lineSeparator()}"
+        val a = SavingsAccount(1000, 2.5)
+        val out = PrinterStringBuffer()
+        val logger = Logger(out, LoggerKind.FUNCTIONS)
+        logger.log(a)
+        assertEquals(expected, out.buffer.toString())
+    }
+
+    @Test fun testLogPointToDefaultOutput() {
+        Logger().log(Student(7613, "Ze Manel", Address(67, "Rua das Papoilas")))
+    }
+}
+

2769295cb44321f1c3e7ff1694510555a3947bb9
Miguel Gamboa <miguelgamboa@outlook.com>
2022-04-19 21:58:40 +0100

    lesson 18 TPC

--- aula17-logger-dynamic/src/test/kotlin/pt/isel/DynamicLoggerTest.kt
+++ aula17-logger-dynamic/src/test/kotlin/pt/isel/DynamicLoggerTest.kt
@@ -3,11 +3,16 @@ package pt.isel
 import org.junit.Test
 import kotlin.reflect.KProperty
 import kotlin.reflect.full.declaredMemberProperties
+import kotlin.test.assertEquals
 
 class DynamicLoggerTest {
 
     @Test fun testDynamicGetter() {
         val bal: KProperty<*> = SavingsAccount::class.declaredMemberProperties.find { it.name == "balance" }!!
-        buildGetterProperty(SavingsAccount::class, bal).writeTo(System.out)
+        val getterBalanceKlassFile = buildGetterProperty(SavingsAccount::class, bal)
+        val out = PrinterStringBuffer()
+        val getterBalance = loadAndCreateInstance(getterBalanceKlassFile, out) as Getter
+        getterBalance.readAndPrint(SavingsAccount(2733, 2.5))
+        assertEquals("balance = 2733,", out.buffer.toString())
     }
 }

4850d240e877b84fe477915403b4fbf4bc2a6dec
Miguel Gamboa <miguelgamboa@outlook.com>
2022-05-02 15:57:01 +0100

    Finish LoggerDynamic

--- aula20-logger-JMH/src/main/kotlin/pt/isel/GetterPropertyBuilder.kt
+++ aula20-logger-JMH/src/main/kotlin/pt/isel/GetterPropertyBuilder.kt
@@ -5,6 +5,7 @@ import com.squareup.javapoet.JavaFile
 import com.squareup.javapoet.MethodSpec
 import com.squareup.javapoet.TypeSpec
 import javax.lang.model.element.Modifier
+import kotlin.reflect.KCallable
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
 import kotlin.reflect.jvm.javaMethod
@@ -27,27 +28,63 @@ fun buildGetterProperty(klass: KClass<*>, prop: KProperty<*>) : JavaFile {
 
     val fieldOut = FieldSpec
         .builder(Printer::class.java, "out")
+        .addModifiers(Modifier.FINAL)
         .build()
 
-    // TPC Constructor to initialize out field.
     val init = MethodSpec
         .constructorBuilder()
-        // ..... // TPC TODO
+        .addModifiers(Modifier.PUBLIC)
+        .addParameter(Printer::class.java, "out" )
+        .addStatement("this.\$N = out", fieldOut)
         .build()
-    //
-    // Object v = ((SavingsAccount) target).getBalance();
-    // out.print("balance = " + v + ",");
-    //
+
     val readAndPrint = MethodSpec
         .methodBuilder("readAndPrint")
         .addModifiers(Modifier.PUBLIC)
         .addParameter(Any::class.java, "target")
-        // .addStatement() // TODO: Object v = ((SavingsAccount) target).getBalance();
-        // .addStatement() // TODO: out.print("balance = " + v + ",");
+        .addStatement("Object v =((${klass.qualifiedName}) target).${prop.getter.javaMethod?.name}()")
+        .addStatement("out.print(\"${prop.name} = \" + v + \",\")")
         .build()
 
     val getter = TypeSpec
         .classBuilder("Getter_Property_${klass.simpleName}_${prop.name}")
+        .addModifiers(Modifier.PUBLIC)
+        .addSuperinterface(Getter::class.java)
+        .addField(fieldOut)
+        .addMethod(readAndPrint)
+        .addMethod(init)
+        .build()
+
+    return JavaFile.builder("", getter).build()
+}
+
+fun buildGetterFunc(klass: KClass<*>, func: KCallable<*>) : JavaFile {
+
+    val fieldOut = FieldSpec
+        .builder(Printer::class.java, "out")
+        .addModifiers(Modifier.FINAL)
+        .build()
+
+    val init = MethodSpec
+        .constructorBuilder()
+        .addModifiers(Modifier.PUBLIC)
+        .addParameter(Printer::class.java, "out" )
+        .addStatement("this.\$N = out", fieldOut)
+        .build()
+
+    val funcName = if(func is KProperty) func.getter.javaMethod?.name
+        else func.name
+
+    val readAndPrint = MethodSpec
+        .methodBuilder("readAndPrint")
+        .addModifiers(Modifier.PUBLIC)
+        .addParameter(Any::class.java, "target")
+        .addStatement("Object v =((${klass.qualifiedName}) target).${funcName}()")
+        .addStatement("out.print(\"${func.name}() = \" + v + \",\")")
+        .build()
+
+    val getter = TypeSpec
+        .classBuilder("Getter_Function_${klass.simpleName}_${func.name}")
         .addModifiers(Modifier.PUBLIC)
         .addSuperinterface(Getter::class.java)
         .addField(fieldOut)
--- aula20-logger-JMH/src/main/kotlin/pt/isel/LoggerDynamic.kt
+++ aula20-logger-JMH/src/main/kotlin/pt/isel/LoggerDynamic.kt
@@ -16,9 +16,9 @@ class LoggerDynamic(
             .declaredMembers
             .filter{ (it.parameters.size == 1) && it.returnType != unitType }
             .filter { it.hasAnnotation<ToLog>() }
-            .map { func -> if(func.findAnnotation<ToLog>()?.formatter == Unit::class)
-                GetterFunction(out, func)
-                else GetterFunctionFormatter(out, func)
+            .map { func ->
+                val getterFile = buildGetterFunc(klass, func)
+                loadAndCreateInstance(getterFile, out) as Getter
             }
     }
 
@@ -26,9 +26,9 @@ class LoggerDynamic(
         return klass
             .memberProperties
             .filter { it.hasAnnotation<ToLog>() }
-            .map { prop -> if(prop.findAnnotation<ToLog>()?.formatter == Unit::class)
-                GetterProperty(out, prop)
-                else GetterPropertyFormatter(out, prop)
+            .map { prop ->
+                val getterFile = buildGetterProperty(klass, prop)
+                loadAndCreateInstance(getterFile, out) as Getter
             }
     }
 }
